2bab6109f1f8840f3f68d83042784bd561f7079f
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-01 21:43:24 +0000

    list changing checked att with map function

--- exercicio2/src/Content.js
+++ exercicio2/src/Content.js
@@ -1,35 +1,55 @@
+import { FaTrashAlt } from 'react-icons/fa'
 import { useState } from 'react'
 
 const Content = () => {
-  const [name, setName] = useState('Alexis');
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "BAnana"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Crema"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "apple"
+    },
+
+  ]);
+
+  const handlechecked = (id) =>{
+    console.log(`key: ${id}`)
+    // const idMinus1 = id -1;
+    // const listItems = [...items]
+    // listItems[idMinus1].checked = !listItems[id].checked;
+    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
+    
+    setItems(listItems);
+  }
+
   const content = {
     textAlign: 'center',
     paddingTop: '20px',
+  }
 
-  }
-  const handleNameChange = () => {
-    const names = ['Alexis', 'Fernando', 'David'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  }
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  }
-  const handleClick2 = () => {
-    console.log(count);
-  }
 
 
   return (
     <main style={content}>
-      <p onDoubleClick={handleClick}>
-        Hello {name} !
-      </p>
-      <button onClick={handleNameChange}> Change Name</button>
-      <button onClick={handleClick}> Count</button>
-      <button onClick={handleClick2}> Count 2</button>
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input type="checkbox" checked={item.checked} onChange={() => handlechecked(item.id)}/>
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0">Delete</FaTrashAlt>
+          </li>
+        ))}
+      </ul>
+
     </main>
   )
 }
