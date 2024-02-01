fdaa68872ad611f208b3a0603881dd3e7d54070f
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-01 21:51:34 +0000

    Deleting an item - adding css when checked

--- exercicio2/src/Content.js
+++ exercicio2/src/Content.js
@@ -21,13 +21,18 @@ const Content = () => {
 
   ]);
 
-  const handlechecked = (id) =>{
+  const handlechecked = (id) => {
     console.log(`key: ${id}`)
     // const idMinus1 = id -1;
     // const listItems = [...items]
     // listItems[idMinus1].checked = !listItems[id].checked;
-    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
-    
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    localStorage.setItem('shopingList', JSON.stringify(listItems));
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
   }
 
@@ -43,9 +48,12 @@ const Content = () => {
       <ul>
         {items.map((item) => (
           <li className='item' key={item.id}>
-            <input type="checkbox" checked={item.checked} onChange={() => handlechecked(item.id)}/>
-            <label>{item.item}</label>
-            <FaTrashAlt role="button" tabIndex="0">Delete</FaTrashAlt>
+            <input type="checkbox" checked={item.checked} onChange={() => handlechecked(item.id)} />
+            <label
+              style={(item.checked) ? {textDecoration: 'line-through' }: null}
+              onDoubleClick={() => handlechecked(item.id)}
+            >{item.item}</label>
+            <FaTrashAlt onClick={() => handleDelete(item.id)} role="button" tabIndex="0">Delete</FaTrashAlt>
           </li>
         ))}
       </ul>
