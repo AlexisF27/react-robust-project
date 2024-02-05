7b0d003310e77eead057631f31fac103dbf2c98b
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-05 22:27:02 +0000

    useRef

--- exercicio2/src/AddItem.js
+++ exercicio2/src/AddItem.js
@@ -1,7 +1,9 @@
 import {FaPlus} from 'react-icons/fa'
 import React from 'react'
+import {useRef} from 'react';
 
 const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+  const inputRef = useRef();
   return (
     <form className='addForm' onSubmit={handleSubmit}>
       <label htmlFor="addItem"> Add Item </label>
@@ -9,11 +11,12 @@ const AddItem = ({newItem, setNewItem, handleSubmit}) => {
         autoFocus
         id='addItem'
         type='text'
+        ref={inputRef}
         placeholder='Add Item'
         required 
         value={newItem}
         onChange={(event) => setNewItem(event.target.value)}/>
-      <button type='submit' aria-label='Add Item'>
+      <button type='submit' aria-label='Add Item' onClick={() => inputRef.current.focus()}>
         <FaPlus></FaPlus>
       </button>
 
