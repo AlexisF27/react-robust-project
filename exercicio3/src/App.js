ebac5f71da52acf3a0d12b3378e7be533efa584f
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-07 18:39:49 +0000

    fetch Data and catching erros

--- exercicio3/src/App.js
+++ exercicio3/src/App.js
@@ -9,18 +9,34 @@ import Header from './Header';
 import SearchItem from './SearchItem';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')) || []);
+  const API_URL = 'http://localhost:3500/items';
+
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
 
-  useEffect(()=> {
-    localStorage.setItem('shoppingList', JSON.stringify(items));
-  },[items])
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not received expected data');
+        const listIems = await response.json();
+        setItems(listIems);
+        setFetchError(null);
+      } catch (error) {
+        console.log(error.message);
+        setFetchError(error.message);
+      }
+    }
+    (async () => await fetchItems())();
+
+  }, [])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
-    const listItems = [ ...items, myNewItem ];
+    const listItems = [...items, myNewItem];
     setItems(listItems);
   }
 
@@ -40,15 +56,28 @@ function App() {
     setNewItem('');
   }
 
-   
+
 
   return (
     <div className='App'>
-      <Header title="Groceries List" />
-      <SearchItem search={search} setSearch={setSearch}></SearchItem>
-      <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit}></AddItem>
-      <Content items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))} handlechecked={handlechecked} handleDelete={handleDelete} />
-      <Footer length={items.length} />
+      <Header
+        title="Groceries List" />
+      <SearchItem
+        search={search}
+        setSearch={setSearch} />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit} />
+      <main>
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handlechecked={handlechecked}
+          handleDelete={handleDelete} />}
+        <Footer
+          length={items.length} />
+      </main>
     </div>
   );
 }
